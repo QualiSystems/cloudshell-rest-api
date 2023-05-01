4bc46925dbfaa50a69a0f94b17b85fcf8157e41c
Kyrylo Maksymenko <saklar13@gmail.com>
2023-05-01 12:00:32 +0200

    add typing

--- cloudshell/rest/models.py
+++ cloudshell/rest/models.py
@@ -1,28 +1,19 @@
+from __future__ import annotations
+
+
 class UserInfo:
-    """User info model.
-
-    :type user_name: str
-    :type email: str
-    """
-
-    def __init__(self, info_dict):
-        self.user_name = info_dict["Username"]
-        self.email = info_dict["Email"]
+    def __init__(self, info_dict: dict):
+        self.user_name: str = info_dict["Username"]
+        self.email: str = info_dict["Email"]
 
     def __repr__(self):
         return "<UserInfo Username:{0.user_name}, Email:{0.email}>".format(self)
 
 
 class ExecutionEnvironmentType:
-    """Execution environment type model.
-
-    :type position: int
-    :type path str
-    """
-
     def __init__(self, info_dict):
-        self.position = info_dict["Position"]
-        self.path = info_dict["Path"]
+        self.position: int = info_dict["Position"]
+        self.path: str = info_dict["Path"]
 
     def __repr__(self):
         return (
@@ -32,30 +23,16 @@ class ExecutionEnvironmentType:
 
 
 class ShellInfo:
-    """Shell info model.
-
-    :type id_: str
-    :type name: str
-    :type version: str
-    :type standard_type: str
-    :type modification_date: str
-    :type last_modified_by_user: UserInfo
-    :type author: str
-    :type is_official: bool
-    :type based_on: str
-    :type execution_environment_type: ExecutionEnvironmentType
-    """
-
     def __init__(self, info_dict):
-        self.id_ = info_dict["Id"]
-        self.name = info_dict["Name"]
-        self.version = info_dict["Version"]
-        self.standard_type = info_dict["StandardType"]
-        self.modification_date = info_dict["ModificationDate"]
+        self.id_: str = info_dict["Id"]
+        self.name: str = info_dict["Name"]
+        self.version: str = info_dict["Version"]
+        self.standard_type: str = info_dict["StandardType"]
+        self.modification_date: str = info_dict["ModificationDate"]
         self.last_modified_by_user = UserInfo(info_dict["LastModifiedByUser"])
-        self.author = info_dict["Author"]
-        self.is_official = info_dict["IsOfficial"]
-        self.based_on = info_dict["BasedOn"]
+        self.author: str = info_dict["Author"]
+        self.is_official: bool = info_dict["IsOfficial"]
+        self.based_on: str = info_dict["BasedOn"]
         self.execution_environment_type = ExecutionEnvironmentType(
             info_dict["ExecutionEnvironmentType"]
         )
@@ -65,15 +42,9 @@ class ShellInfo:
 
 
 class StandardInfo:
-    """Standard Info model.
-
-    :type standard_name: str
-    :type versions: list[str]
-    """
-
     def __init__(self, info_dict):
-        self.standard_name = info_dict["StandardName"]
-        self.versions = info_dict["Versions"]
+        self.standard_name: str = info_dict["StandardName"]
+        self.versions: list[str] = info_dict["Versions"]
 
     def __repr__(self):
         return "<StandardInfo Name:{0.standard_name}, Versions:{0.versions}>".format(
